7cbface189aebb62fdf9b6c9314f1c5562e0c2c6
vidyamusic <vidyadharmusic@gmail.com>
2024-07-21 23:47:11 +0530

    auth & authorization passport.js

--- models/Person.js
+++ models/Person.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 //Define the Person Schema
 
 const personSchema = new mongoose.Schema({
@@ -32,8 +32,42 @@ const personSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
+personSchema.pre("save", async function (next) {
+  const person = this;
+  //Hash the password only if it has been modified (or it's new)
+  if (!person.isModified("password")) return next();
+  try {
+    //hash password generation
+    const salt = await bcrypt.genSalt(10);
+    //hash password
+    const hashedPassword = await bcrypt.hash(person.password, salt);
+    //override the plain password with the hashed one
+    person.password = hashedPassword;
+  } catch (err) {
+    return next(err);
+  }
+});
+//creating comparison function for the auth.js
+personSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    //use bcrypt to compare the provided password with the hashed password
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    throw err;
+  }
+};
 //Create Person Model
 
 const Person = mongoose.model("Person", personSchema);
--- server.js
+++ server.js
@@ -4,18 +4,32 @@ const db = require("./db");
 const Person = require("./models/Person");
 const MenuItem = require("./models/MenuItem");
 const bodyParser = require("body-parser");
+const passport = require("./auth");
+//Initializing passport
+app.use(passport.initialize());
 //Importing Router Files -
 const personRoutes = require("./routes/personRoutes");
 const menuRoutes = require("./routes/menuRoutes");
 //Importing .env for sensitive info
 require("dotenv").config();
 app.use(bodyParser.json()); //stores all data in req.body
+//Middleware example function -
+const logRequest = (req, res, next) => {
+  console.log(
+    `[${new Date().toLocaleString()}] Request Made to: ${req.originalUrl}`
+  );
+  next(); //Move on to the next phase.
+};
+//pass the logRequest (middleware function) to the get function.
+const localAuthMiddleware = passport.authenticate("local", { session: false });
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
-
+//in order for the whole app to use the middleware we use -
+app.use(logRequest);
+//in order to use only for one route , we can add middleware into the given use
 //Using the Person Router
-app.use("/person", personRoutes);
+app.use("/person", localAuthMiddleware, personRoutes);
 //Using the Menu Router
 app.use("/menu", menuRoutes);
 //using .env variable
